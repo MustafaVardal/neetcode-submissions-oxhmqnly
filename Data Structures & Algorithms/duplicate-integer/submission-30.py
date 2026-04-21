b42f9383a919766db223f51398ed4105bcfc2453
Matt Mustafa Vardal <137339044+MustafaVardal@users.noreply.github.com>
2026-04-21 13:54:32 -0400

    Add: duplicate-integer - submission-30

---
+++ Data Structures & Algorithms/duplicate-integer/submission-30.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        unique = set()
+
+        for num in nums:
+            if num not in unique:
+                unique.add(num)
+        print(unique)
+        print(nums)
+        return len(unique) != len(nums)
